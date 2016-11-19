eff1b9b84aec2db8af55c5cfa950a3d41f3bbe7b
finderlo <www.houyudong@qq.com>
2016-11-19 11:23:11 +0800

    11-19-L1 apply-pop

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,4 +1,3 @@
-
 window.onload = function () {
     document.body.style.fontSize = screen.width * 0.01 + "px";
 };
@@ -14,4 +13,22 @@ function setVisible(bool) {
 
 function alert_info(info) {
     alert(info);
+}
+
+function openPopAdd(code, name) {
+
+    var popAdd = document.getElementsByClassName('pop_add-bg');
+    var a = document.getElementsByName("itemCode");
+    var b = document.getElementsByName("itemName");
+    // alert("code:" + code + "name:" + name + "length:" + popAdd.length+"itemCode"+b.length);
+    // var itemCode = popAdd[0].getElementsByName("itemCode");
+    // var itemName = popAdd[0].getElementsByName("itemName");
+    a[1].value = code;
+    b[1].value = name;
+    popAdd[0].style.display = "block";
+}
+
+function closePopAdd() {
+    var popAdd = document.getElementsByClassName('pop_add-bg');
+    popAdd[0].style.display = "none";
 }
