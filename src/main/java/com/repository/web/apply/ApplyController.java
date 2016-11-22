cc53ac4ee09dd3938b1d53417cef55f9bd9aabe5
finderlo <www.houyudong@qq.com>
2016-11-22 23:56:42 +0800

    builer166

--- src/main/java/com/repository/web/apply/ApplyController.java
+++ src/main/java/com/repository/web/apply/ApplyController.java
@@ -3,11 +3,14 @@ package com.repository.web.apply;
 import com.repository.base.BaseController;
 import com.repository.dao.ItemApplicationDao;
 import com.repository.dao.ItemApplicationOperationDao;
+import com.repository.entity.ItemApplicationOperationEntity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 import static com.repository.Constants.*;
 @Controller
@@ -22,7 +25,10 @@ public class ApplyController extends BaseController {
     @RequestMapping(URL_APPLY)
     public String apply(Model model) {
         logger.trace("apply/");
-        model.addAttribute("applications", applicationOperationDao.findAll());
+        List<ItemApplicationOperationEntity> entities = applicationOperationDao.findAll();
+        model.addAttribute("history", entities);
+        logger.info("apply: applications size:" + entities.size());
+        entities.forEach(entity -> logger.info("apply: id:" + entity.getApplicationId()));
         return HTML_APPLY_HISTORY;
     }
 }
